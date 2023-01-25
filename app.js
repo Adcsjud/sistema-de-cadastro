d092802f52a237d623d4ff159505f7f2303fa37d
Adcsjud <sanjudodaniel@gmail.com>
2023-01-24 21:30:20 -0300

    Testando o resrtore

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+console.log("Adicionando mais um console")
+
+console.log("Mais um de teste")
